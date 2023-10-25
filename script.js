beeb59becc1c878830bdf15a9b9e166b9d834541
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-25 18:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
 function twoCompare(){
-	const number = prompt("Enter the Inputs");
-	let a= 10;
-	let b = 10 ;   
+	const number1 = prompt("Enter the First Number");
+	const number2 = prompt("Enter the Second Number");
+	
+	let a= parseFloat(number1);
+	let b = parseFloat(number2);   
  
 	if(a === b)
 	{
