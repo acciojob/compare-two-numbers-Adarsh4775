936ff2ae23f4fddb6c2f8a91c20bf670c1564462
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-25 18:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function twoCompare(){
+	const number = prompt("Enter the Inputs");
+	let a= 10;
+	let b =10 ;   
+ 
+	if(a === b)
+	{
+		alert("true");
+	}
+	else{
+		alert("false");
+	}
+	}       
+twoCompare();
